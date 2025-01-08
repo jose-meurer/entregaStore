dc947b2d64ab4566b8f024a596a6254a7caa5ca5
Jose Meurer <josewmeurer@gmail.com>
2025-01-08 15:08:20 -0300

    Primeiro commit

---
+++ src/main/java/com/josemeurer/entregaStore/EntregaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.entregaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemeurer/entregaStore/EntregaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemeurer.entregaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
